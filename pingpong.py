a624d836d45daadf37259aee656ae8a0dfe2a5ea
Philip <philip.patrick.goebel@gmail.com>
2026-03-07 13:12:29 +0100

    Update pingpong.py

--- pingpong.py
+++ pingpong.py
@@ -25,15 +25,15 @@ class GameSprite(sprite.Sprite):
 
 class Player(GameSprite):
     def __init__(self, x, y, up_key, down_key):
-        super().__init__("ping_pong_ball.png", x, y, 50, 150, 4)
+        super().__init__("ping_pong_ball.png", x, y, 50, 150, 5)
         self.up_key = up_key
         self.down_key = down_key
 
     def update(self):
         keys = key.get_pressed()
-        if keys[self.up_key] and self.rect.x > 5:
+        if keys[self.up_key] and self.rect.y > 5:
             self.rect.y -= self.speed
-        if keys[self.down_key] and self.rect.x < win_width - 5:
+        if keys[self.down_key] and self.rect.y < win_height - self.rect.height - 5:
             self.rect.y += self.speed
 
 class Ball(GameSprite):
@@ -63,7 +63,7 @@ player_left = Player(50, win_height//2 - 75, K_UP, K_DOWN)
 player_right = Player(win_width - 100, win_height//2 - 75, K_w, K_s)
 
 run = True
-FPS = 20
+FPS = 60
 clock = time.Clock()
 finish = False
 
@@ -71,12 +71,13 @@ while run:
     for e in event.get():
         if e.type == QUIT:
              run = False
-        if not finish:
-            window.blit(background, (0,0))
-            player_left.update()
-            player_right.update()
-            player_left.draw()
-            player_right.draw()
+    
+    if not finish:
+        window.blit(background, (0,0))
+        player_left.update()
+        player_right.update()
+        player_left.draw()
+        player_right.draw()
 
     display.update()
     clock.tick(FPS)
